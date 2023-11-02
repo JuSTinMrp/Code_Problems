fba86fe55e1bc5e4b74f2a4ffa0d5b3399cca13c
JuSTinMrp <r4mp3nt3st3r619@gmail.com>
2023-11-02 14:01:16 +0530

    added_new

---
+++ POP-2/under2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+
+for (int i=65;i<=90;i++)
+{
+printf("%c",i);
+//printf("\n");
+}
+printf("\n");
+for (int i=1;i<=26;i++)
+{
+printf("%d",i/10);
+//printf("\n");
+}
+printf("\n");
+
+for (int i=1;i<=26;i++)
+{
+printf("%d",i%10);
+}
+
+
+//for (int i=1;i<=26;i++)
+//{
+//printf("~");
+//}
+
+
+
+}
