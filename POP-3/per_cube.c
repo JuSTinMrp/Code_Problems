c2e1ce6c8e63dfa927831e3460bf3eefe6eed40a
JuSTinMrp <r4mp3nt3st3r619@gmail.com>
2023-11-03 14:45:39 +0530

    perfect_cube

---
+++ POP-3/per_cube.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int n,flag;
+    scanf("%d",&n);
+
+    if (n==0 || n==1){
+        // printf("True");
+        flag=1;
+    }
+    else{
+        // printf("True");
+        for (int i=2;i<=n/2;i++){
+            if (n==(i*i*i)){
+                // printf("True");
+                flag=1;
+                // printf("%d",flag);
+                break;
+            }
+            else{
+                flag=0;
+            }
+        }
+    }
+    // printf("%d",flag);
+    if (flag==1){
+        printf("%d is perfect cube",n);}
+    else{
+        printf("%d is not a perfect cube",n);
+
+    }
+
+    }
