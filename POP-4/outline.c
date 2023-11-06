7c58f2c6c9c944e6ae6bff9da64cb43fafe95ee5
JuSTinMrp <r4mp3nt3st3r619@gmail.com>
2023-11-06 16:48:30 +0530

    new_updates

---
+++ POP-4/outline.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int N,star,space;
+    star=1;
+    space=4;
+    scanf("%d",&N);
+    for (int i=1;i<=(2*N-1);i++)    //row
+    {
+        for (int j=1;j<=space;j++){ //space
+            printf(" ");
+    }
+        for (int k=1;k<=star;k++){
+            if ((k==1) || )
+        }
+    }
+}
